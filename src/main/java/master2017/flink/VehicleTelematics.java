1b7b6ff490ea537f3c7b003f959736118dab9740
Giorgio Ruffa <gioruffa@gmail.com>
2017-12-18 17:54:43 +0100

    CarEvent stream

--- src/main/java/master2017/flink/VehicleTelematics.java
+++ src/main/java/master2017/flink/VehicleTelematics.java
@@ -1,8 +1,97 @@
 package master2017.flink;
 
+import master2017.flink.events.CarEvent;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.functions.timestamps.AscendingTimestampExtractor;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import scala.Tuple2;
+
+import java.io.File;
+
 public class VehicleTelematics {
+    static String inputFilePath;
+    static String outputDirectoryPath;
+    static Logger logger = LoggerFactory.getLogger(VehicleTelematics.class);
+
     static public void main(String  [] args)
     {
-        System.out.println("Hola Mundo!");
+        if (!checkArgs(args)) {
+            return;
+        }
+
+        //TODO: come back to store only string not files
+        inputFilePath = args[0];
+        outputDirectoryPath = args[1];
+
+        final StreamExecutionEnvironment streamEnv = StreamExecutionEnvironment.getExecutionEnvironment();
+        streamEnv.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        streamEnv.setParallelism(1);
+
+
+        DataStreamSource<String> fileStreamSource = streamEnv.readTextFile(inputFilePath);
+
+        SingleOutputStreamOperator<CarEvent> carEventStream = fileStreamSource.flatMap(new FlatMapFunction<String, CarEvent>() {
+            @Override
+            public void flatMap(String s, Collector<CarEvent> collector) throws Exception {
+                try {
+                    collector.collect(CarEvent.fromString(s));
+                } catch (Exception ex) {
+                    //in case of malformed lines
+                    //TODO: make an exception custom class
+                    ex.printStackTrace();
+                }
+            }
+        }).assignTimestampsAndWatermarks(
+                new AscendingTimestampExtractor<CarEvent>() {
+                    @Override
+                    public long extractAscendingTimestamp(CarEvent carEvent) {
+                        return carEvent.getTimestamp() * 1000;
+                    }
+                }
+        );
+
+        carEventStream.map(new MapFunction<CarEvent, CarEvent>() {
+            @Override
+            public CarEvent map(CarEvent carEvent) throws Exception {
+                System.out.println(carEvent.toString());
+                return carEvent;
+            }
+        });
+
+        try {
+            streamEnv.execute();
+        } catch (Exception e) {
+            logger.error("Execption: " , e);
+        }
+
+
     }
+
+    static boolean checkArgs(String [] args)
+    {
+        if (args.length != 2)
+        {
+            System.err.println("Not enough arguments provided");
+            return false;
+        }
+        if (!(new File(args[0]).exists()))
+        {
+            System.err.println("Input file does not exists");
+            return false;
+        }
+        if (!(new File(args[1])).isDirectory())
+        {
+            System.err.println("Second argument should be a directory");
+            return false;
+        }
+        return true;
+    }
+
 }
---
+++ src/main/java/master2017/flink/events/CarEvent.java
@@ -0,0 +1,142 @@
+package master2017.flink.events;
+
+/**
+ * Commodity POJO for the input line
+ */
+public class CarEvent {
+    Long timestamp;
+    String vehicleID;
+    Integer speedModule; //speed in physics is an array, we are just considering the module. It has only pos. values
+    String highwayID; //string because it is not clear the value it can have
+    Short lane; //just from 0 to 4
+    Boolean westbound; //TODO: consider to use an enum, how does flink take it for partitioning?
+    Integer segment;
+    Integer position;
+
+    public CarEvent(
+            Long timestamp,
+            String vehicleID,
+            Integer speedModule,
+            String highwayID,
+            Short lane,
+            Boolean westbound,
+            Integer segment,
+            Integer position) {
+        this.timestamp = timestamp;
+        this.vehicleID = vehicleID;
+        this.speedModule = speedModule;
+        this.highwayID = highwayID;
+        this.lane = lane;
+        this.westbound = westbound;
+        this.segment = segment;
+        this.position = position;
+    }
+
+    public Integer getSegment() {
+        return segment;
+    }
+
+    public void setSegment(Integer segment) {
+        this.segment = segment;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(String vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public Integer getSpeedModule() {
+        return speedModule;
+    }
+
+    public void setSpeedModule(Integer speedModule) {
+        this.speedModule = speedModule;
+    }
+
+    public String getHighwayID() {
+        return highwayID;
+    }
+
+    public void setHighwayID(String highwayID) {
+        this.highwayID = highwayID;
+    }
+
+    public Short getLane() {
+        return lane;
+    }
+
+    public void setLane(Short lane) {
+        this.lane = lane;
+    }
+
+    public Boolean getWestbound() {
+        return westbound;
+    }
+
+    public void setWestbound(Boolean westbound) {
+        this.westbound = westbound;
+    }
+
+    @Override
+    public String toString() {
+        return "CarEvent{" +
+                "timestamp=" + timestamp +
+                ", vehicleID='" + vehicleID + '\'' +
+                ", speedModule=" + speedModule +
+                ", highwayID='" + highwayID + '\'' +
+                ", lane=" + lane +
+                ", westbound=" + westbound +
+                ", segment=" + segment +
+                ", position=" + position +
+                '}';
+    }
+
+    /**
+     * Create a CarEvent from line of csv
+     * Since costructure never should throw exceptions, we use a stitic method to validate and create the class.
+     * Handle exceptions accordingly
+     * @param inputOrigin
+     * @return
+     * @throws Exception
+     */
+    public static CarEvent fromString(String inputOrigin) throws Exception {
+        String[] tokens = inputOrigin.split(",");
+        if (tokens.length != 8)
+        {
+            throw new Exception("Malformed input line");
+        }
+
+        return new CarEvent(
+                Long.valueOf(tokens[0]),
+                tokens[1],
+                Integer.valueOf(tokens[2]),
+                tokens[3],
+                Short.valueOf(tokens[4]),
+                Integer.valueOf(tokens[5]) == 0 ? Boolean.FALSE : Boolean.TRUE,
+                Integer.valueOf(tokens[6]),
+                Integer.valueOf(tokens[7])
+        );
+
+
+    }
+
+}
